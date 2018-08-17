1e3bdb32459abb58e974722eca857c0347e1d1fd
Thomas Flack <thomas.flack@skybettingandgaming.com>
2018-08-17 18:49:08 +0100

    Port purge command to new command struct

--- bot.js
+++ bot.js
@@ -18,17 +18,6 @@ client.on('message', async message => {
   if (client.commands.has(command))
     return client.commands.get(command).run(message);
 
-  if (command === 'purge') {
-    const deleteCount = parseInt(args[0], 10);
-
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    return message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
-
   if (command === 'whoami') {
     const m = `You are ${message.author}, duh!`;
     return message.channel.send(m);
---
+++ src/command/commands/Purge.js
@@ -0,0 +1,23 @@
+const Command = require('../Command.js');
+
+class Purge extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: 'purge',
+      description: 'Deletes a number of messages from the channel',
+    });
+  }
+
+  async run(message) {
+    const deleteCount = parseInt(args[0], 10);
+
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    return message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+}
+
+module.exports = Purge;
