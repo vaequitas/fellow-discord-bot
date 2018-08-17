c3ea70c878dc08abc085dcd347c1b8cd7a1258c1
Thomas Flack <thomas.flack@skybettingandgaming.com>
2018-08-17 14:05:41 +0100

    Add purge command

--- bot.js
+++ bot.js
@@ -18,12 +18,23 @@ client.on('message', async message => {
 
   if (command === 'ping') {
     const m = await message.channel.send('Ping?');
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
+    return m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
   }
 
   if (command === 'source') {
     const m = "My source code is at https://github.com/vaequitas/fellow-discord-bot :heart:";
-    message.channel.send(m);
+    return message.channel.send(m);
+  }
+
+  if (command === 'purge') {
+    const deleteCount = parseInt(args[0], 10);
+
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    return message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 });
 
