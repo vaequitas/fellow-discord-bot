aaea9db8eb9ebd6b38fcd40d5ce7aa6c90aabe17
nonki <thomas@flack.it>
2018-08-18 19:43:43 +0100

    Initialize database object in client

--- src/client/FellowsClient.js
+++ src/client/FellowsClient.js
@@ -1,11 +1,19 @@
 const { Client } = require('discord.js');
 const CommandStore = require('../command/CommandStore.js');
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+var serviceAccount = require("../../fellows-discord-bot-firebase-adminsdk-kriks-628e35a1d2.json");
 
 class FellowsClient extends Client {
   constructor(options) {
     super(options);
 
     this.commands = new CommandStore(this);
+
+    this.firebase = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://fellows-discord-bot.firebaseio.com"
+    });
   }
 
   async init() {
