ce818d8d75bf95f78d528ac51a66f8f2f34a3f07
nonki <thomas@flack.it>
2018-08-25 17:53:53 +0100

    [subcommands] Moved profile search subcommand to its own class

--- src/command/commands/anilist/Profile.js
+++ src/command/commands/anilist/Profile.js
@@ -1,7 +1,5 @@
 const Command = require('../../Command.js');
 const CommandStore = require('../../CommandStore.js');
-const fetch = require('node-fetch');
-const UserProvider = require('../../../providers/User.js');
 const path = require('path');
 
 class Profile extends Command {
@@ -20,8 +18,6 @@ class Profile extends Command {
       enabled: true,
     });
 
-    this.user = new UserProvider(this.client.firebase.database());
-
     this.subcommands = new CommandStore(this.client, {
       dir: `${path.dirname(require.main.filename)}${path.sep}src${path.sep}command${path.sep}commands${path.sep}anilist${path.sep}profile`,
       parent: this.name
@@ -33,55 +29,6 @@ class Profile extends Command {
     if (args.length && this.subcommands.has(args[0])) {
       const command = args.shift().toLowerCase();
       return await this.subcommands.get(command).run(message, args)
-    }
-
-    if (args[0] !== 'search')
-      return message.channel.send(this.getHelp());
-
-    var query = `
-    query ($search: String) {
-      User (search: $search) {
-        name
-        siteUrl
-      }
-    }
-    `;
-
-    var variables = {
-        search: args[1]
-    };
-
-    var url = 'https://graphql.anilist.co',
-        options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({
-                query: query,
-                variables: variables
-            })
-        };
-
-    fetch(url, options).then(handleResponse)
-                       .then(handleData)
-                       .catch(handleError);
-
-    function handleResponse(response) {
-      return response.json().then(json => {
-        return response.ok ? json : Promise.reject(json);
-      });
-    }
-
-    function handleData(data) {
-      const user = data.data.User;
-      const m = `I found the user ${user.name}: ${user.siteUrl}`;
-      return message.reply(m);
-    }
-
-    function handleError(error) {
-      console.error('Error searching for user', error);
     }
   }
 }
