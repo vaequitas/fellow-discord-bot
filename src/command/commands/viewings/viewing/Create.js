d9994931c5021534dafc82ddf251f72bb6d23af0
nonki <thomas@flack.it>
2018-08-26 16:47:48 +0100

    [create_hosts] Add viewing create subcommand to create viewings

---
+++ src/command/commands/viewings/viewing/Create.js
@@ -0,0 +1,50 @@
+const Command = require('../../../Command.js');
+const ViewingProvider = require('../../../../providers/Viewing.js');
+const Viewing = require('../../../../structures/Viewing.js');
+const chrono = require('chrono-node');
+
+class Create extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: 'create',
+      enabled: true,
+    });
+    this.provider = new ViewingProvider(this.client.firebase.database());
+    this.parent = 'viewing'
+  }
+
+  async run(message, args) {
+    const full_message = args.join(' ');
+    const results      = await chrono.parse(full_message);
+    if (!results.length)
+      return
+
+    const result = results.shift();
+    const timezoneOffset = result.start.get('timezoneOffset');
+    if (!timezoneOffset)
+      result.start.assign('timezoneOffset', 0);
+
+    const date = new Date(result.start.date());
+    if (!date) return
+
+    const viewing = new Viewing({
+      date: date.toISOString(),
+      host: message.author.id,
+    });
+    const dateString = date.toUTCString();
+    const new_message = await message.reply(`are you sure you want to host a viewing on ${dateString}? React to this message to confirm`);
+    await new_message.react('☑');
+
+    const filter = (reaction, user) => reaction.emoji.name === '☑' && user.id === message.author.id
+    new_message.awaitReactions(filter, {time: 15000, max: 1})
+      .then(async collected => {
+        if (!collected.size)
+          return message.reply('confirmation timed out, cancelling creation.');
+
+        const viewingKey = await this.provider.save(viewing);
+        return await message.reply(`succesfully created viewing ${viewingKey}`);
+      }).catch(console.error);
+  }
+}
+
+module.exports = Create;
